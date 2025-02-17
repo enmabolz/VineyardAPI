6428dfcb836502a40f56df115011d68682c039d0
Enmanuel Bolzonello <enm.bolz@gmail.com>
2025-02-17 22:26:47 +0100

    Adding Services

---
+++ VineyardAPI/Services/IManagerService.cs
@@ -0,0 +1,11 @@
+﻿using VineyardManager.Models;
+
+namespace VineyardManager._Services
+{
+    public interface IManagerService
+    {
+        Task<IEnumerable<Guid>> GetIdsOfManagersAsync(); 
+        Task<IEnumerable<string>> GetTaxNumbersOrderedAsync(bool sorted);
+
+    }
+}
---
+++ VineyardAPI/Services/ManagerService.cs
@@ -0,0 +1,33 @@
+﻿using VineyardManager.Repositories;
+
+namespace VineyardManager._Services
+{
+    public class ManagerService : IManagerService
+    {
+        private readonly IManagerRepository _managerRepository;
+
+        public ManagerService(IManagerRepository managerRepository)
+        {
+            _managerRepository = managerRepository;
+        }
+
+        public async Task<IEnumerable<Guid>> GetIdsOfManagersAsync()
+        {
+            var managers = await _managerRepository.GetAllManagersAsync();
+
+            return managers.Select(x => x.Id);
+        }
+
+        public async Task<IEnumerable<string>> GetTaxNumbersOrderedAsync(bool sorted)
+        {
+            var managers = await _managerRepository.GetAllManagersAsync();
+
+            if (sorted)
+            {
+                return managers.OrderBy(x => x.Name).Select(x => x.TaxNumber);
+            }
+
+            return managers.Select(x => x.TaxNumber);
+        }
+    }
+}
