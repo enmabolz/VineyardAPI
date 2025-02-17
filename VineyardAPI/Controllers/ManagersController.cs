6fbd8492c8a8702fe2a00034469bbf3ab10cf01d
Enmanuel Bolzonello <enm.bolz@gmail.com>
2025-02-17 22:27:46 +0100

    Adding ManagersController

---
+++ VineyardAPI/Controllers/ManagersController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VineyardManager._Services;
+
+namespace VineyardManager.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ManagersController : ControllerBase
+    {
+        private readonly IManagerService _managerService;
+        public ManagersController(IManagerService managerService)
+        {
+            _managerService = managerService;
+        }
+
+        [HttpGet("ids")]
+        public async Task<IActionResult> GetAllIdsOfManagersAsync() 
+        {
+            var managerIds = await _managerService.GetIdsOfManagersAsync();
+
+            return Ok(managerIds);
+        }
+
+        [HttpGet("taxnumbers")]
+        public async Task<IActionResult> GetAllTaxNunmbersFromManagersAsync([FromQuery] bool sorted) 
+        {
+            var taxNumbers = await _managerService.GetTaxNumbersOrderedAsync(sorted);
+            
+            return Ok(taxNumbers);
+        }
+
+    }
+}
