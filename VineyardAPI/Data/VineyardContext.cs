243b88c35b05b096a2cc9bb02cfc26b0df21acdf
Enmanuel Bolzonello <enm.bolz@gmail.com>
2025-02-17 22:26:13 +0100

    Adding Context to use it with EF

---
+++ VineyardAPI/Data/VineyardContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VineyardManager.Models;
+
+namespace VineyardManager.Data
+{
+    public class VineyardContext : DbContext
+    {
+        public VineyardContext(DbContextOptions<VineyardContext> options): base(options) {}
+
+        public DbSet<Manager> Managers { set; get; }
+        public DbSet<Grape> Grapes { get; set; }
+        public DbSet<Vineyard> Vineyards { get; set; }
+        public DbSet<Parcel> Parcels { get; set; }
+    }
+}
