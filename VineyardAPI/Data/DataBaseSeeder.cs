62ecb8c28e72063a77cd2b627ee2d7119496f85c
Enmanuel Bolzonello <enm.bolz@gmail.com>
2025-02-18 09:39:43 +0100

    Adding Database Seeder class

---
+++ VineyardAPI/Data/DataBaseSeeder.cs
@@ -0,0 +1,79 @@
+﻿using VineyardAPI.Models;
+
+namespace VineyardAPI.Data
+{
+    public class DataBaseSeeder
+    {
+        private readonly VineyardContext _context;
+
+        public DataBaseSeeder(VineyardContext context) 
+        {
+            _context = context;
+        }
+
+        public void Seed()
+        {
+           
+            if (!_context.Managers.Any())
+            {
+                SeedManagers();
+            }
+
+            if (!_context.Grapes.Any())
+            {
+                SeedGrapes();
+            }
+
+            if (!_context.Vineyards.Any())
+            {
+                SeedVineyards();
+            }
+
+            _context.SaveChanges();
+
+            if (!_context.Parcels.Any())
+            {
+                SeedParcels();
+            }
+
+            _context.SaveChanges();
+        }
+
+        private void SeedManagers()
+        {
+            _context.Managers.AddRange(
+                new Manager { TaxNumber = "132254524", Name = "Miguel Torres" },
+                new Manager { TaxNumber = "143618668", Name = "Ana Martín" },
+                new Manager { TaxNumber = "78903228", Name = "Carlos Ruiz" }
+            );
+        }
+
+        private void SeedGrapes()
+        {
+            _context.Grapes.AddRange(
+                new Grape { Name = "Tempranillo" },
+                new Grape { Name = "Albariño" },
+                new Grape { Name = "Garnacha" }
+            );
+        }
+
+        private void SeedVineyards()
+        {
+            _context.Vineyards.AddRange(
+                new Vineyard { Name = "Viña Esmeralda" },
+                new Vineyard { Name = "Bodegas Bilbaínas" }
+            );
+        }
+
+        private void SeedParcels()
+        {
+            _context.Parcels.AddRange(
+                new Parcel { ManagerId = 1, VineyardId = 1, GrapeId = 1, YearPlanted = 2019, Area = 1500 },
+                new Parcel { ManagerId = 2, VineyardId = 2, GrapeId = 2, YearPlanted = 2021, Area = 9000 },
+                new Parcel { ManagerId = 3, VineyardId = 1, GrapeId = 1, YearPlanted = 2020, Area = 3000 },
+                new Parcel { ManagerId = 1, VineyardId = 2, GrapeId = 2, YearPlanted = 2020, Area = 2000 },
+                new Parcel { ManagerId = 3, VineyardId = 2, GrapeId = 2, YearPlanted = 2021, Area = 1000 }
+            );
+        }
+    }
+}
--- VineyardAPI/Program.cs
+++ VineyardAPI/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
-using VineyardManager.Data;
+using VineyardAPI.Data;
+using VineyardAPI.Repositories;
+using VineyardAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,7 +15,20 @@ builder.Services.AddDbContext<VineyardContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
 
+builder.Services.AddTransient<DataBaseSeeder>();
+
+builder.Services.AddScoped<IManagerRepository, ManagerRepository>();
+builder.Services.AddScoped<IManagerService, ManagerService>();
+
+
 var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var seeder = scope.ServiceProvider.GetRequiredService<DataBaseSeeder>();
+    seeder.Seed();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
