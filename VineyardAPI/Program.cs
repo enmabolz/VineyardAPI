3bd749e097a22b7476106d594e11a565b6ad261a
Enmanuel Bolzonello <enm.bolz@gmail.com>
2025-02-19 18:35:38 +0100

    Adding docker

--- VineyardAPI/Program.cs
+++ VineyardAPI/Program.cs
@@ -13,6 +13,19 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
+builder.Services.AddControllers();
+
 builder.Services.AddDbContext<VineyardContext>(
     options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
     );
@@ -28,14 +41,14 @@ builder.Services.AddScoped<IGrapeService, GrapeService>();
 builder.Services.AddScoped<IVineyardService, VineyardService>();
 
 
-
-
 var app = builder.Build();
-
 
 using (var scope = app.Services.CreateScope())
 {
+    var context = scope.ServiceProvider.GetRequiredService<VineyardContext>();
     var seeder = scope.ServiceProvider.GetRequiredService<DataBaseSeeder>();
+
+    context.Database.Migrate();
     seeder.Seed();
 }
 
